9230baccc2c0e3c585af7bd26914b2e6e685fac9
tesla9512 <tesla9512@gmail.com>
2022-05-10 00:09:46 +0900

    main에서 spring주석

--- src/main/java/com/baseball/core/CoreApplication.java
+++ src/main/java/com/baseball/core/CoreApplication.java
@@ -1,13 +1,15 @@
 package com.baseball.core;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
+		//SpringApplication.run(CoreApplication.class, args);
+		//java로 이루어진 코드이므로 스프링을 사용하지않는다
+		System.out.println("TEST");
 	}
 
 }
